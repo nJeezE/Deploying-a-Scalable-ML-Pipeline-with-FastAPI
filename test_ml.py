6b7e91557e548e528e659123036ef3ebc5483482
nJeezE <nickgonzalez.flair@gmail.com>
2026-07-12 22:38:06 -0500

    unit testing completed, screenshot added

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,67 @@
 import pytest
 # TODO: add necessary import
+import numpy as np
+import os
+from ml.data import apply_label
+from ml.model import train_model, save_model, load_model, compute_model_metrics
+
+from sklearn.ensemble import RandomForestClassifier
+
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_apply_label_input():
     """
-    # add description for the first test
+    # testing that the input successfully maps 0 and 1
     """
-    # Your code here
-    pass
-
+    # force a 0 through the pipeline
+    result_zero = apply_label(np.array([0]))
+    assert result_zero == "<=50K"
+    
+    # force a 1 through the pipeline
+    result_one = apply_label(np.array([1]))
+    assert result_one == ">50K"
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_save_load(tmp_path):
     """
-    # add description for the second test
+    # tests that the model is saved and reloaded as a random forest classifier
     """
-    # Your code here
-    pass
+    # dummy dataset
+    X_train = np.random.rand(15, 5)
+    y_train = np.random.randint(0, 2, size=15)
+
+    # train the model
+    original_model = train_model(X_train, y_train)
+
+    # create tmp_path
+    temp_file_path = os.path.join(tmp_path, "model.pkl")
+
+    # save the model
+    save_model(original_model, temp_file_path)
+
+    assert os.path.exists(temp_file_path)
+    assert os.path.getsize(temp_file_path) > 0
+
+    # loading the model back in for testing
+    loaded_model = load_model(temp_file_path)
+
+    # check that the model exists and is a random forest classifier
+    assert isinstance(loaded_model, RandomForestClassifier)
+
+
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_metrics_shape():
     """
-    # add description for the third test
+    # test that the metrics return the correct values for perfect prediction
     """
-    # Your code here
-    pass
+    # dummy data
+    y_true = np.array([0, 1, 0, 1])
+    y_pred = np.array([0, 1, 1, 0])
+    # calculate metrics
+    metrics = compute_model_metrics(y_true, y_pred)
+    # returns tuple of length 3
+    assert isinstance(metrics, tuple)
+    assert len(metrics) == 3
